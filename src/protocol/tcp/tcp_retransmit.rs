3ea4379c0d183a83636bcafc9838659d670206b3
Jeremy Kong <weituo_kong@brown.edu>
2022-04-28 21:00:15 -0400

    small update on check_retransmit

--- src/protocol/tcp/tcp_retransmit.rs
+++ src/protocol/tcp/tcp_retransmit.rs
@@ -16,20 +16,31 @@ use crate::protocol::{
  * - pending_socks: set of all sockets with pending operation
  *
  * Returns:
- * - 
+ * -
  */
 pub fn check_retransmission(
     sockets: SocketTable,
     pending_socks: Arc<DashSet<SocketEntry>>,
-)-> thread::JoinHandle<()> {
+) -> thread::JoinHandle<()> {
     thread::spawn(move || {
         loop {
             // iterate pending_socks
             for sock_entry in pending_socks.iter() {
-                let sock = sockets.get_socket_by_entry(&sock_entry);
+                let sock = sockets.get_socket_by_entry(&sock_entry)?;
+                match sock.get_tcp_state() {
+                    TCPState::SynSent => (),
+                    TCPState::SynRcvd => (),
+                    TCPState::Established
+                    | TCPState::FinWait1
+                    | TCPState::CloseWait
+                    | TCPState::Closing => (),
+                    TCPState::TimeWait => (),
+                    TCPState::LastAck => (),
+                    _ => (),
+                }
             }
-            
-            thread::sleep(Duration::from_millis(1));   
+
+            thread::sleep(Duration::from_millis(1));
         }
     })
 }
